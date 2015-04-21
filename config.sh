a0aad6de7338fdb589d95ffcd55b225aac47a833
Jake Russo <madcap.russo@gmail.com>
2015-04-21 00:30:46 -0500

    Fix jndi-name

--- config.sh
+++ config.sh
@@ -22,8 +22,22 @@ $JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG &
 echo "==> Waiting..."
 wait_for_wildfly
 
+# --file=`dirname "$0"`/batch.cli
 echo "==> Executing jdbc driver script..."
-$JBOSS_CLI -c --file=`dirname "$0"`/batch.cli
+$JBOSS_CLI -c << EOF
+batch
+
+# Add Postgres module
+module add --name=org.postgresql --resources=/tmp/psql-jdbc.jar:/tmp/postgis-jdbc.jar --dependencies=javax.api,javax.transaction.api
+
+# Add Postgres driver
+/subsystem=datasources/jdbc-driver=postgresql:add(driver-name=postgresql,driver-module-name=org.postgresql,driver-xa-datasource-class-name=org.postgresql.xa.PGXADataSource)
+
+# Add datasource
+data-source add --name=PostgrePool --jndi-name=java:jboss/datasources/PGDS --connection-url=jdbc:postgresql://$PG_PORT_5432_TCP_ADDR:$PG_PORT_5432_TCP_PORT --driver-name=postgresql --user-name=postgres
+
+run-batch
+EOF
 
 # echo "==> Removing old snapshots..."snapshots...
 # $JBOSS_CLI -c ":delete-snapshot(name=\"all\")"
